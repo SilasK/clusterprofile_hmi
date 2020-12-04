0a30376194d356b3d812aacf0a94a659368bad59
Silas Kieser <SilasK@users.noreply.github.com>
2020-12-04 10:53:41 +0100

    20 attemps before raising error

--- {{cookiecutter.profile_name}}/lsf_status.py
+++ {{cookiecutter.profile_name}}/lsf_status.py
@@ -5,12 +5,29 @@ import os
 import sys
 import warnings
 import subprocess
+import time
+import logging
+logger = logging.getLogger("__name__")
 
+STATUS_ATTEMPTS = 20
 
 jobid = sys.argv[1]
 
-out= subprocess.run(['bjobs','-noheader',jobid],stdout=subprocess.PIPE).stdout.decode('utf-8')
-
+for i in range(STATUS_ATTEMPTS):
+    try:
+        out= subprocess.run(['bjobs','-noheader',jobid],stdout=subprocess.PIPE).stdout.decode('utf-8')
+        break; # If sucessful go out of for loop
+           
+    except sp.CalledProcessError as e:
+        logger.error("sacct process error")
+        logger.error(e)
+           
+        if i >= STATUS_ATTEMPTS - 1:
+            print("failed")
+            exit(0)
+        else:
+            time.sleep(5)
+                      
 state = out.strip().split()[2]
 
 
