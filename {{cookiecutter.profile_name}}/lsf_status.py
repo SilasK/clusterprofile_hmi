0c50f8593c53d412c4104933a5599b086b6ef0a3
Silas Kieser <SilasK@users.noreply.github.com>
2021-01-20 15:21:30 +0100

    Update lsf_status.py

--- {{cookiecutter.profile_name}}/lsf_status.py
+++ {{cookiecutter.profile_name}}/lsf_status.py
@@ -9,7 +9,7 @@ import time
 import logging
 logger = logging.getLogger("__name__")
 
-STATUS_ATTEMPTS = 20
+STATUS_ATTEMPTS = 10
 
 jobid = sys.argv[1]
 
@@ -18,18 +18,24 @@ for i in range(STATUS_ATTEMPTS):
         out= subprocess.run(['bjobs','-noheader',jobid],stdout=subprocess.PIPE).stdout.decode('utf-8')
         break; # If sucessful go out of for loop
            
-    except sp.CalledProcessError as e:
+    except subprocess.CalledProcessError as e:
         logger.error(f"CLUSTER: can't get status for job {jobid}")
         logger.error(e)
            
         if i >= STATUS_ATTEMPTS - 1:
+            logger.error(f"CLUSTER: tried {STATUS_ATTEMPTS}, mark as failed.")
             print("failed")
             exit(0)
         else:
+            logger.info("CLUSTER: wait and try again")
             time.sleep(5)
-                      
-state = out.strip().split()[2]
-
+try:                      
+    state = out.strip().split()[2]
+except Exception as e:
+    logger.error(f"CLUSTER: Couln't decode bjobs output: {out}")
+    logger.error(e)
+    print("failed")
+    exit(0)
 
 map_state={"PEND":'running',
            "RUN":'running', 
